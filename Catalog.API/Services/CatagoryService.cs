98bf8f30d01362613c6e41c3c39c81c64981dcb7
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-27 16:56:26 +0300

    add category service

---
+++ Catalog.API/Services/CatagoryService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Catalog.API.Configurations;
+using Catalog.API.Dtos;
+using Catalog.API.Models;
+using MongoDB.Driver;
+using Utilities.Results;
+
+namespace Catalog.API.Services
+{
+    internal class CatagoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CatagoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task<ApiResponse<List<CategoryDto>>> GetAllAsync()
+        {
+            var categories = await _categoryCollection.Find(c => true).ToListAsync();
+
+            return ApiResponse<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
+        }
+
+        public async Task<ApiResponse<CategoryDto>> CreateAsync(Category category)
+        {
+            await _categoryCollection.InsertOneAsync(category);
+
+            return ApiResponse<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+
+        public async Task<ApiResponse<CategoryDto>> GetByIdAsync(string id)
+        {
+            var category = await _categoryCollection.Find(c => c.Id == id).FirstOrDefaultAsync();
+
+            if(category is null)
+            {
+                return ApiResponse<CategoryDto>.Fail(404, "Category not found");
+            }
+
+            return ApiResponse<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+    }
+}
---
+++ Catalog.API/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Dtos;
+using Catalog.API.Models;
+using Utilities.Results;
+
+namespace Catalog.API.Services
+{
+    internal interface ICategoryService
+    {
+        Task<ApiResponse<List<CategoryDto>>> GetAllAsync();
+        Task<ApiResponse<CategoryDto>> CreateAsync(Category category);
+        Task<ApiResponse<CategoryDto>> GetByIdAsync(string id);
+    }
+}
