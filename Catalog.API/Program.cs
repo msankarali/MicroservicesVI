9ca01cf6815d7a4ed9c39b1009e310ae21d41973
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-27 16:37:08 +0300

    add db options pattern

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,8 +1,18 @@
+using Catalog.API.Configurations;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
