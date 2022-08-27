6b9ee6200d29d332a6b66cfef0a04f62443cb0ed
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-27 18:04:04 +0300

    add base controller

---
+++ src/Shared/Common/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utilities.Results;
+
+namespace Common
+{
+    public class BaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(ApiResponse<T> apiResponse)
+        {
+            return new ObjectResult(apiResponse)
+            {
+                StatusCode = apiResponse.StatusCode
+            };
+        }
+    }
+}
