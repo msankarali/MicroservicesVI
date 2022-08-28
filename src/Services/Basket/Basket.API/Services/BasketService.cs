cc647f678ec0d38d4609922c866ad27ed7b71eae
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 17:37:17 +0300

    add basket service implementation

---
+++ src/Services/Basket/Basket.API/Services/BasketService.cs
@@ -0,0 +1,46 @@
+﻿using Basket.API.Dtos;
+using System.Text.Json;
+using Utilities.Results;
+
+namespace Basket.API.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<ApiResponse<bool>> DeleteAsync(string userId)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+
+            return status
+                ? ApiResponse<bool>.Success(204)
+                : ApiResponse<bool>.Fail(404, "Basket not found");
+        }
+
+        public async Task<ApiResponse<BasketDto>> GetAsync(string userId)
+        {
+            var basket = await _redisService.GetDb().StringGetAsync(userId);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return ApiResponse<BasketDto>.Fail(404, "Basket not found");
+            }
+
+            return ApiResponse<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(basket), 200);
+        }
+
+        public async Task<ApiResponse<bool>> SaveOrUpdateAsync(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status
+                ? ApiResponse<bool>.Success(204)
+                : ApiResponse<bool>.Fail(500, "Basket could not be updated");
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Services/IBasketService.cs
+++ src/Services/Basket/Basket.API/Services/IBasketService.cs
@@ -1,9 +1,4 @@
 ﻿using Basket.API.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Utilities.Results;
 
 namespace Basket.API.Services
