5fa2367a7854776fda62bec2995a5bbd5c7f21f0
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 13:58:11 +0300

    add AddResourceOwnerValidator for validating valid user

---
+++ src/IdentityServer/IdentityServerV4/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel;
+using IdentityServer4.Validation;
+using IdentityServerV4.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServerV4.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var appUser = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (appUser == null)
+            {
+                var errors = new Dictionary<string, object>
+                {
+                    { "errors", new List<string> { "Wrong email or password" } }
+                };
+
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(appUser, context.Password);
+
+
+            if (!passwordCheck)
+            {
+                var errors = new Dictionary<string, object>
+                {
+                    { "errors", new List<string> { "Wrong email or password" } }
+                };
+
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            context.Result = new GrantValidationResult(
+                subject: appUser.Id.ToString(),
+                authenticationMethod: OidcConstants.AuthenticationMethods.Password);
+        }
+    }
+}
--- src/IdentityServer/IdentityServerV4/Startup.cs
+++ src/IdentityServer/IdentityServerV4/Startup.cs
@@ -5,6 +5,7 @@
 using IdentityServer4;
 using IdentityServerV4.Data;
 using IdentityServerV4.Models;
+using IdentityServerV4.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -54,6 +55,8 @@ namespace IdentityServerV4
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
