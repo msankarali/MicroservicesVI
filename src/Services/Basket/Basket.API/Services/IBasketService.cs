f6c177d1d9d60004c9cc6b6ba31f25997eea1831
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 17:27:23 +0300

    add redis service and implementation

---
+++ src/Services/Basket/Basket.API/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using Basket.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.Results;
+
+namespace Basket.API.Services
+{
+    public interface IBasketService
+    {
+        Task<ApiResponse<BasketDto>> GetAsync(string userId);
+        Task<ApiResponse<bool>> SaveOrUpdateAsync(BasketDto basketDto);
+        Task<ApiResponse<bool>> DeleteAsync(string userId);
+    }
+}
