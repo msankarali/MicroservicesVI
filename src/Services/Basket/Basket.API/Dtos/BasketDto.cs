756e4bd751bb807b127171e9354153b5890835ed
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 17:16:45 +0300

    add basket dtos

---
+++ src/Services/Basket/Basket.API/Dtos/BasketDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+        public decimal TotalPrice
+        {
+            get => BasketItems.Sum(bi => bi.Price * bi.Quantity);
+        }
+    }
+}
