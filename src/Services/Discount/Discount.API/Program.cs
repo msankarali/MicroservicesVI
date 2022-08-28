af72e77c814f6d08836f2cde0323e31684f563b9
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 22:13:02 +0300

    add discount service

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,5 @@
+using Discount.API.Queries;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDiscountCommandText, PostgreDiscountCommandText>();
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var app = builder.Build();
 
---
+++ src/Services/Discount/Discount.API/Services/IDiscountService.cs
@@ -0,0 +1,14 @@
+﻿using Utilities.Results;
+
+namespace Discount.API.Services
+{
+    public interface IDiscountService
+    {
+        Task<ApiResponse<List<Entities.Discount>>> GetAll();
+        Task<ApiResponse<Entities.Discount>> GetById(int id);
+        Task<ApiResponse<NoContent>> Add(Entities.Discount discount);
+        Task<ApiResponse<NoContent>> Update(Entities.Discount discount);
+        Task<ApiResponse<NoContent>> DeleteById(int id);
+        Task<ApiResponse<Entities.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
