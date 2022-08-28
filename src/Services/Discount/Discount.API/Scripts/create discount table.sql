7a6b521ffd001fe943e10418e4a60c0508bc8e1c
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-28 23:21:25 +0300

    edit allow multiple discount code definition for users

--- src/Services/Discount/Discount.API/Scripts/create discount table.sql
+++ src/Services/Discount/Discount.API/Scripts/create discount table.sql
@@ -1,6 +1,6 @@
 ﻿create table Discount(
 	id serial primary key,
-	user_id varchar(100) unique not null,
+	user_id varchar(100) not null,
 	rate smallint not null,
 	code varchar(30) not null,
 	created_date timestamp not null default CURRENT_TIMESTAMP
