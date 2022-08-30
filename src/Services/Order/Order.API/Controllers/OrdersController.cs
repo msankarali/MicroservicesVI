6fbda2f7328e46b759c6f0e5e39383637bae2f47
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-30 18:52:06 +0300

    edit wrong naming in route pattern

--- src/Services/Order/Order.API/Controllers/OrdersController.cs
+++ src/Services/Order/Order.API/Controllers/OrdersController.cs
@@ -10,7 +10,7 @@ using Utilities.Results;
 
 namespace Order.API.Controllers
 {
-    [Route("api/[controller]/[actions]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class OrdersController : BaseController
     {
