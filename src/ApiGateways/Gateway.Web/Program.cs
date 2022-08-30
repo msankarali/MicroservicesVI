5bb501e26e521e20029f5882c47f05956680b775
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-31 01:07:11 +0300

    add ocelot configurations

--- src/ApiGateways/Gateway.Web/Program.cs
+++ src/ApiGateways/Gateway.Web/Program.cs
@@ -2,8 +2,13 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureAppConfiguration((hostingContext, configuration) =>
+{
+    configuration.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json").AddEnvironmentVariables();
+});
 
 builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 await app.UseOcelot();
