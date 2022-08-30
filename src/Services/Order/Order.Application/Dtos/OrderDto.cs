85dd01f57f492e31c452b4907bc68cbde6a3d300
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-30 17:01:16 +0300

    add dtos

---
+++ src/Services/Order/Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; private set; }
+        public AddressDto Address { get; private set; }
+        public string BuyerId { get; private set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
