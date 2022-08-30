aaf32dfc2661e87c9c6dc552618060672f0c80dc
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-08-30 18:49:03 +0300

    remove privates

--- src/Services/Order/Order.Application/Dtos/OrderDto.cs
+++ src/Services/Order/Order.Application/Dtos/OrderDto.cs
@@ -3,9 +3,9 @@
     public class OrderDto
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; private set; }
-        public AddressDto Address { get; private set; }
-        public string BuyerId { get; private set; }
+        public DateTime CreatedDate { get; set; }
+        public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
         public List<OrderItemDto> OrderItems { get; set; }
     }
 }
