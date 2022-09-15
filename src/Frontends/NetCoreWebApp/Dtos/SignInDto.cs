443f90da3d1e4d782941943cfc6e2b4f5ad0879f
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-09-15 19:27:48 +0300

    add abstract signin operations and models

---
+++ src/Frontends/NetCoreWebApp/Dtos/SignInDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreWebApp.Dtos
+{
+    public class SignInDto
+    {
+        [Display(Name = "E-mail address")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ src/Frontends/NetCoreWebApp/Services/Abstract/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel.Client;
+using NetCoreWebApp.Dtos;
+using Utilities.Results;
+
+namespace NetCoreWebApp.Services.Abstract
+{
+    public interface IIdentityService
+    {
+        Task<ApiResponse<bool>> SignIn(SignInDto signInDto);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
