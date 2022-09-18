d27096f63e861e8680bbac3bd3ae6f245ac59426
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2022-09-18 20:00:02 +0300

    add signin page and signin nav bar

---
+++ src/Frontends/NetCoreWebApp/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreWebApp.Dtos;
+
+namespace NetCoreWebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
